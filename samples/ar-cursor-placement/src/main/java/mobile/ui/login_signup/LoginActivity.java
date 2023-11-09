ad61b0236a5a5b52c5da85e8ec947a37b7160e4d
YangJK <wlyjk123456@gmail.com>
2023-11-09 21:04:05 +1100

    change of login

--- samples/ar-cursor-placement/src/main/java/mobile/ui/login_signup/LoginActivity.java
+++ samples/ar-cursor-placement/src/main/java/mobile/ui/login_signup/LoginActivity.java
@@ -1,5 +1,6 @@
 package mobile.ui.login_signup;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -7,11 +8,24 @@ import android.widget.EditText;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.widget.Toast;
 
-import java.io.DataOutputStream;
+import android.app.ProgressDialog; // Add this import statement
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.io.InputStreamReader;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -20,22 +34,19 @@ public class LoginActivity extends AppCompatActivity {
     private Button loginButton;
     private TextView signUpTextView;
     private TextView forgotPasswordTextView;
+    private ProgressDialog mDialog; // Add this line for ProgressDialog
 
-    private static HttpURLConnection connection = null;
-    private static DataOutputStream out = null;
-    private static InputStream in = null;
-    private static URL realUrl = null;
-    private String params = "";//定义参数字符串
+    private String params = ""; //定义参数字符串
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_home);
 
-        emailEditText = findViewById(R.id.editTextEmail);
-        passwordEditText = findViewById(R.id.editTextPassword);
-        loginButton = findViewById(R.id.buttonLogin);
-        signUpTextView = findViewById(R.id.textViewSignUp);
+        emailEditText = findViewById(R.id.edit_email);
+        passwordEditText = findViewById(R.id.edit_senha);
+        loginButton = findViewById(R.id.bt_entrar);
+        signUpTextView = findViewById(R.id.bt_signup);
         forgotPasswordTextView = findViewById(R.id.textViewForgotPassword);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
@@ -44,17 +55,14 @@ public class LoginActivity extends AppCompatActivity {
                 String email = emailEditText.getText().toString();
                 String password = passwordEditText.getText().toString();
 
-                // Check if email and password are valid (add your authentication logic here)
+                // Check if email and password are valid
                 if (isValid(email, password)) {
-                    // Continue with the login process
                     login(email, password);
                 } else {
-                    // Show an error message or handle invalid credentials
                     Toast.makeText(LoginActivity.this, "Invalid email or password", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-
 
         signUpTextView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,5 +85,80 @@ public class LoginActivity extends AppCompatActivity {
         // For simplicity, this example checks if both fields are non-empty
         return !TextUtils.isEmpty(email) && !TextUtils.isEmpty(password);
     }
+
+    private void login(String username, String password) {
+        mDialog = new ProgressDialog(LoginActivity.this);
+        mDialog.setTitle("Login...");
+        mDialog.setMessage("Waiting...");
+        mDialog.setCancelable(false);
+        mDialog.show();
+
+        JSONObject paramsObject = new JSONObject();
+        try {
+            paramsObject.put("address", username); // Assuming username is the email
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        params = paramsObject.toString();
+
+        LoginTask task = new LoginTask();
+        task.execute();
+    }
+
+    public class LoginTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            requestModel(params);
+            return null;
+        }
+    }
+
+    private void requestModel(String address) {
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(getApplicationContext());
+        String url = "https://mobiles-2a62216dada4.herokuapp.com/location/layout";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // response
+                        Log.e("requestTest", "Model: " + response.substring(0, Math.min(response.length(), 50)));
+
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
+                            String model = jsonObject.getString("model");
+
+                            // Handle the model response as needed
+                        } catch (JSONException ex) {
+                            throw new RuntimeException(ex);
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("requestTest", "Error: " + error.getMessage());
+                        // deal with error response
+                    }
+                }) {
+            @Override
+            public byte[] getBody() {
+                return params.getBytes();
+            }
+
+            @Override
+            public String getBodyContentType() {
+                return "application/json; charset=utf-8";
+            }
+        };
+
+        // add to queue
+        queue.add(stringRequest);
+    }
 }
+
+
 
